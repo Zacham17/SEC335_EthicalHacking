e41f4f55787aac3c798a102f1894e066974206ca
Zacham17 <zachary.morris@mymail.champlain.edu>
2022-09-17 13:33:38 -0400

    Added Powershell DNS Resolver

---
+++ week03/dns-resolver.ps1
@@ -0,0 +1,8 @@
+﻿param($network, $server)
+for ($num = 1 ; $num -le 254 ; $num++){
+    if ($name = Resolve-DnsName -DnsOnly "$network.$num" -Server $server -ErrorAction Ignore | Select-Object -Property NameHost)
+    {
+        $newname = $name -replace "@{NameHost=" -replace "}"
+        Write-Host "$network.$num $newname"
+    }
+}
